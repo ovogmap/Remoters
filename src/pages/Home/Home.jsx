eeee5b2d5baf0c091264503a61e8410c9d1def6d
ovogmap <djtheend@naver.com>
2021-06-17 23:31:37 +0900

    인트로 컴포넌트 작성 완료

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,6 +7,9 @@ import { Button } from '../../components'
 import useFetchJobs from '../../hooks/useFetchJobs'
 
 import logo from '../../assets/images/remoters.png'
+import remoters from '../../assets/images/remoters@3x.png'
+import main from '../../assets/images/main.png'
+import search from '../../assets/images/search.png'
 
 export default function Home() {
   const { jobs, loading, err } = useFetchJobs()
@@ -15,6 +18,12 @@ export default function Home() {
   const handleCreateCompany = () => {
     history.push('/company')
   }
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault()
+    window.alert('검색버튼 눌림')
+  }
+
   if (loading) return <div>...loading</div>
   if (err) return <div>...err</div>
   return (
@@ -30,6 +39,21 @@ export default function Home() {
               onClick={handleCreateCompany}
             />
           </Header>
+          <Section>
+            <SectionLeft>
+              <div>
+                <img src={remoters} alt="" />
+                <p>[명사] 시간과 공간의 제약을 받지않고 일하는 사람</p>
+              </div>
+              <Input onSubmit={handleSearchSubmit}>
+                <input type="text" />
+                <button type="submit" />
+              </Input>
+            </SectionLeft>
+            <SectionRight>
+              <img src={main} alt="" />
+            </SectionRight>
+          </Section>
         </Inner>
       </Intro>
     </Container>
@@ -64,4 +88,81 @@ const Header = styled.header`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
+`
+
+const Section = styled.section`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding-top: 88px;
+
+  img {
+    max-width: 467px;
+  }
+`
+
+const SectionLeft = styled.div`
+  height: 385px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+
+  p {
+    font-size: 30px;
+    font-weight: 500;
+  }
+`
+
+const Input = styled.form`
+  width: 454px;
+  height: 76px;
+  background: #fff;
+  border-radius: 38px;
+  box-shadow: 0 6px 39px 0 rgba(165, 165, 165, 0.31);
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 23px 32px;
+
+  input {
+    border: none;
+    background: initial;
+    height: 100%;
+    flex: 1;
+    font-size: 24px;
+    font-weight: 600;
+    outline: none;
+  }
+
+  button {
+    width: 50px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+    background-image: url(${search});
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-color: inherit;
+    border: none;
+    cursor: pointer;
+  }
+`
+
+const SectionRight = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+
+  img {
+    width: 481px;
+    height: 385px;
+  }
 `
