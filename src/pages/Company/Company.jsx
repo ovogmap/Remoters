201054452116eacb98842d2b1c02fcc5b403b09e
ovogmap <djtheend@naver.com>
2021-06-18 03:37:23 +0900

    회사 등록하기 페이지 작성

--- src/pages/Company/Company.jsx
+++ src/pages/Company/Company.jsx
@@ -1,5 +1,49 @@
 import React from 'react'
+import styled from 'styled-components'
+import { useHistory } from 'react-router-dom'
+
+import { Button } from '../../components'
 
 export default function SearchResult() {
-  return <div>SearchResult</div>
+  const history = useHistory()
+
+  const handleGoBack = () => {
+    history.goBack()
+  }
+  return (
+    <Container>
+      <Inner>
+        <h1>회사 등록 페이지</h1>
+        <Button
+          innerText="뒤로가기"
+          width={250}
+          height={60}
+          onClick={handleGoBack}
+        />
+      </Inner>
+    </Container>
+  )
 }
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(98, 115, 232, 0.1);
+`
+
+const Inner = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    margin-bottom: 20px;
+  }
+`
